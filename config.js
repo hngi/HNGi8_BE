f2b0f197bd719ce39fbd251dbb24890266d8f836
tonymj76 <demeyortj@gmail.com>
2020-07-23 22:05:21 +0100

    config file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: process.env.PORT || 5600,
+  host: process.env.HOST || 'localhost',
+  dbPass: process.env.PASS || 'hngi8be',
+  dbname: process.env.NAME || 'hngi8be',
+  dbconnection: process.env.DBURL
+}
