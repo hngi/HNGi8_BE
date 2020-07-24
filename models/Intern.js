7b5ed5336ae0cbc851d9eead3c5c5cd09b764530
Zeeson <olabrazanislam@gmail.com>
2020-07-24 14:37:27 -0700

    features: Interns Model

--- models/Intern.js
+++ models/Intern.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
@@ -56,27 +55,3 @@ applicationState: {
 });
 
 module.exports = mongoose.model('Interns', internSchema);
-=======
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema
-
-const internLoginSchema = Schema({
-
-    email: {
-        type: String,
-        required: true,
-        unique: true
-        },
-   
-    password: {
-        type: String,
-        required: true
-    }
-   
-})
-
-const InternLogin = mongoose.model("InternLogin", internLoginSchema);
-
-module.exports = InternLogin; 
->>>>>>> models
