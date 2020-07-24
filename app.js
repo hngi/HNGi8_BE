cf044ac8d20134d0055a0f9ba4f991c71780ac47
tonymj76 <demeyortj@gmail.com>
2020-07-24 12:17:39 +0100

    add view setups

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
+const path = require('path');
+
 const homeRouter = require('./routes/routes');
 const { handleError } = require('./utils/error');
+const viewRouter = require('./routes/views/viewRoutes');
 
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// All routes goes here
+/// api routes which will be irrivent now
+app.use('/v1', homeRouter);
+/// view routes
+app.use('/', viewRouter);
+
 // Express error middleware
 app.use((err, req, res, next) => {
   next(handleError(res, err));
 });
-app.use('/v1', homeRouter);
+
 // Unknown endpoints middleware
 app.use('*', (req, res) => {
   const url = req.originalUrl;
