783cd5abbf300daae50921d3e478f3200452e24d
Zeeson <olabrazanislam@gmail.com>
2020-07-24 13:01:59 -0700

    feature: Intern Models

---
+++ models/Intern.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const internSchema = Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  internId:{
+    type: Number,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phoneNumber: {
+    type: Number,
+    required: true
+  },
+  cvLink: {
+    type: String,
+    required: true,
+    default: 'blank'
+  },
+  interest: {
+    type: String, 
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String
+  }, 
+  employmentStatus: {
+    type: String,
+    enum: ['employed', 'unemployed', 'student'],
+    default: 'unemployed'
+  },
+  about: {
+    type: String
+  },
+  registrationDate:{
+    type:Date,
+    default:Date.now
+},
+applicationState: {
+  type: String,
+  enum: ['accepted', 'declined', 'pending'],
+  default: 'pending'
+},
+});
+
+module.exports = mongoose.model('Interns', internSchema);
