72effac50c0ef058b8dc25279dd8935d670c83b6
Aptcoder <omilosamuel@gmail.com>
2020-07-24 15:29:44 +0100

    chore: refactor model

---
+++ models/Mentor.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const mentorSchema = Schema({
+
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone_number: {
+    type: Number,
+    required: true
+  },
+  cv_link: {
+    type: String,
+    default: 'blank'
+  },
+
+  employed_status: {
+    type: String,
+    enum: ['employed', 'unemployed', 'student'],
+    required: true,
+    default: 'unemployed'
+  },
+  country: {
+    type: String
+  },
+  state_of_residence: {
+    type: String
+  },
+
+  interest: {
+    type: String
+  }
+}, {
+  timestamp: true
+});
+
+module.exports = mongoose.model('Mentor', mentorSchema);
