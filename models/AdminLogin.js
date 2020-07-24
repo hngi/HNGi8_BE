9ea4db81a06d08bf65ae201acf01bc084aee5ade
tonymj76 <demeyortj@gmail.com>
2020-07-24 20:36:45 +0100

    admin login models

--- models/AdminLogin.js
+++ models/AdminLogin.js
@@ -4,7 +4,14 @@ const uniqueValidator = require('mongoose-unique-validator');
 const adminSchema = mongoose.Schema({
   name: { type: String, require: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ['superAdmin', 'admin'],
+    default: 'admin'
+  }
+}, {
+  timestamps: true
 });
 
 adminSchema.plugin(uniqueValidator);
