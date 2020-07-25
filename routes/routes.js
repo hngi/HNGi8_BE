35a54754aaa1d464fe4759967a8102c86b5fd520
Aptcoder <omilosamuel@gmail.com>
2020-07-25 19:49:06 +0100

    feat: add feature for mentor application

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const multer = require('multer');
 const { homePage } = require('../controller');
 
-const { mentorApplication, applicationValidationRules} = require('../controller/mentor');
+const { mentorApplication, applicationValidationRules } = require('../controller/mentor');
 
+const upload = multer();
 const router = express.Router();
 
 router.get('/', homePage);
-router.get('/test', applicationValidationRules(), mentorApplication);
+router.post('/mentor/apply', upload.none(), applicationValidationRules(), mentorApplication);
 module.exports = router;
