a5c874fce23d4be914d0d6c6fd2d695bf3f67f08
Timothy Mayor <timothymayor24@gmail.com>
2020-07-25 19:32:16 +0100

    created internController and modified the routes

--- routes/views/viewRoutes.js
+++ routes/views/viewRoutes.js
@@ -13,4 +13,35 @@ viewRouter.get('/intern', intern);
 
 viewRouter.get('/mentor', mentor);
 
+
+
+/// INTERN ROUTES ///
+
+// // GET request for creating Intern. 
+// router.get('/intern/create', intern_controller.intern_create_get);
+
+// // POST request for creating Intern.
+// router.post('/intern/create', intern_controller.intern_create_post);
+
+// // GET request to delete Intern.
+// router.get('/intern/:intern_id/delete', intern_controller.intern_delete_get);
+
+// // POST request to delete Intern
+// router.post('/intern/:intern_id/delete', intern_controller.intern_delete_post);
+
+// // GET request to update Intern.
+// router.get('/intern/:intern_id/update', intern_controller.intern_update_get);
+
+// // POST request to update Intern.
+// router.post('/intern/:intern_id/update', intern_controller.intern_update_post);
+
+// // GET request for one Intern.
+// router.get('/intern/:intern_id', intern_controller.intern_detail);
+
+// // GET request for list of all Employees.
+// router.get('/interns', intern_controller.intern_list);
+
+
+
+
 module.exports = viewRouter;
