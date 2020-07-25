52761724ce6a8888bfd33c8fad9b601da5ef1467
Aptcoder <omilosamuel@gmail.com>
2020-07-25 18:26:28 +0100

    chore: mod Mentor model for required validation

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const { homePage } = require('../controller');
 
+const { mentorApplication, applicationValidationRules} = require('../controller/mentor');
+
 const router = express.Router();
 
 router.get('/', homePage);
-
+router.get('/test', applicationValidationRules(), mentorApplication);
 module.exports = router;
