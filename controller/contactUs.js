25b35c71593e773825d2dfdcb21c64e04de17f5b
Zeeson <olabrazanislam@gmail.com>
2020-07-26 16:20:28 -0700

    feat: contactUs api

---
+++ controller/contactUs.js
@@ -0,0 +1,32 @@
+const Contacts = require("../models/Contact")
+
+// post 
+const contact = (req, res) => {
+
+    const name = req.body.name
+    const email = req.body.email
+    const subject = req.body.subject
+    const message = req.body.message
+    
+    const contacts  = {
+        name,  
+        email,
+        subject,
+        message
+      } 
+
+  Contacts.create(contacts)
+  .then(contacts, () => {
+    req.flash('success', 'Thanks for contacting us...');
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err);
+    req.flash('error', 'comment not saved please try again');
+    res.redirect('/contact');
+});
+}; 
+
+
+module.exports = {
+        contact
+  }
