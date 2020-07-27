0021c113c5a3087f73d206805e193d8797473cff
tonymj76 <demeyortj@gmail.com>
2020-07-27 12:54:47 +0100

    log out route

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { homePage, login, contact } = require('../controller');
+const { homePage, login, contact, logout } = require('../controller');
 
 const { mentorApplication, applicationValidationRules } = require('../controller/mentor');
 
@@ -12,6 +12,7 @@ router.get('/', homePage);
 router.post('/mentors/apply', applicationValidationRules(), mentorApplication);
 router.post('/auth', login);
 router.post('/contact-us', contact);
+router.get('/logout', logout);
 router.post('/apply', internValidationRules(), internApply);
 
 module.exports = router;
