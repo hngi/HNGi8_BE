27951d47f65b6f491ca79dec058d6d4f7b786862
Timothy Mayor <timothymayor24@gmail.com>
2020-07-29 11:30:59 +0100

    modified username in intern model

--- models/Intern.js
+++ models/Intern.js
@@ -62,12 +62,12 @@ const internApplicationSchema = Schema({
     required: false
   },
 
-username: {
-  type: String,
-  required: false,
-  index: {unique: false},
-  minlength: 3
-}
+// username: {
+//   type: String,
+//   required: false,
+//   index: {unique: false},
+//   minlength: 3
+// }
 
 });
 
